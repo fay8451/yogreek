ed817e50d290a363e47e43aee2271eb239a618c3
fay8451 <punnarat.kal@dome.tu.ac.th>
2025-05-13 23:31:01 +0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, './'),
+    };
+    return config;
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
